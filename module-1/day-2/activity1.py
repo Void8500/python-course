5d308ee1de1557998291624140147c1dc4b58deb
Void8500 <alnimrawiqais@gmail.com>
2025-04-29 18:55:55 +0400

    Added day 2 activity 1

---
+++ module-1/day-2/activity1.py
@@ -0,0 +1,8 @@
+name = input("what is your name? ")
+age = input("how old are you? ")
+city = input('where are you from? ')
+print(name, "is only" ,age, "years old")
+print(f"{name} is only {age} years old lives in {city}")
+
+# qais is only 12 years old
+# Abdul is only 13 years old
