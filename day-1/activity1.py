393acb606955538d08db650f06200bee77e5c748
Void8500 <alnimrawiqais@gmail.com>
2025-04-29 18:26:32 +0400

    first-commit
    Co-authored-by: omlanfz <omlanfz@users.noreply.github.com>

---
+++ day-1/activity1.py
@@ -0,0 +1,13 @@
+# Greeting
+print("Hello! My name is Qais")
+
+# My Age
+print("I am 12 years old")
+
+# Fun Fact
+print("Did you know russia is the biggest country in the world?")
+
+# Hobbies
+print(" one of my favorite hobbies is swimming")
+
+print('hello qais')
