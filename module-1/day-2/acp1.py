3bab8fbf02571a8db095f54d3a46cab7eafd0c69
Void8500 <alnimrawiqais@gmail.com>
2025-05-03 22:54:42 +0400

    fourth commit

---
+++ module-1/day-2/acp1.py
@@ -0,0 +1,12 @@
+# Start text
+print("Storing birthdays")
+
+# Bday variables
+bday1 = input("input 1st birthday ")
+bday2 = input("input 2nd birthday ")
+bday3 = input("input 3rd birthday ")
+bday4 = input("input 4th birthday ")
+bday5 = input("input 5th birthday ")
+
+# Showing all birthdays
+print("birthdays: ", bday1,bday2,bday3,bday4,bday5)
