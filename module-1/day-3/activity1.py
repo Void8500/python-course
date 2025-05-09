e0f90d803dd1f778073ebd778fe047a74a4de899
Void8500 <alnimrawiqais@gmail.com>
2025-05-09 14:05:46 +0400

    Added activity 1 and 2 for day 3

---
+++ module-1/day-3/activity1.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+prof = input("What is your profession? ")
+city = input("What city do you live in? ")
+gpa = float(input("What is your GPA? "))
+
+print(f'name: {name}, age: {age}, profession: {prof}, city: {city}')
+
+print(type(name)) 
+print(type(age)) 
+print(type(gpa))
---
+++ module-1/day-3/activity2.py
@@ -0,0 +1,13 @@
+# string[start:stop:step]
+#string slicing
+text = "Python Programming"
+print(text[0:6:1])  # Output: Python
+print(text[6::1])  # Output: Programming
+print(text[0:15:2])  # Output: Pto rgam
+print(text[::-1])  # Output: gnimmargorP nothyP
+print(text[::-2])  # Output: 
+
+# string concatenation
+str1 = 'Qais '
+str2 = "Learning Python"
+print(str1 + str2)
