476f25500899489d8c66f648612ab5815628e103
Void8500 <alnimrawiqais@gmail.com>
2025-05-23 15:12:01 +0400

    added acp1 for day 5

--- module-1/day-5/acp1.py
+++ module-1/day-5/acp1.py
@@ -1,8 +1,8 @@
 temperature = int(input("Enter the temperature in celsius: "))
 
-if temperature >= 25:
+if temperature >= 28:
     print("Its warm. You can wear light clothes.")
-elif temperature >= 15:
+elif temperature >= 17:
     print("Its a bit cold. You can wear a light jacket.")
 else:
     print("Its cold. Wear heavy clothes like a hoodie or jacket.")
