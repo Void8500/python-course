8b522ab9d951f6d6d81992694774b480e5e99862
Void8500 <alnimrawiqais@gmail.com>
2025-06-05 21:11:02 +0400

    Added bonus homework for day 8

--- module-1/day-8/HW.py
+++ module-1/day-8/HW.py
@@ -1,1 +1,19 @@
-# HW: take 3 inputs from the user and find the maximum number and minimum number
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+maximum = num1
+minimum = num1
+
+if num2 > maximum:
+    maximum = num2
+if num3 > maximum:
+    maximum = num3
+
+if num2 < minimum:
+    minimum = num2
+if num3 < minimum:
+    minimum = num3
+    
+print("Maximum number:", maximum)
+print("Minimum number:", minimum)
