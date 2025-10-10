074641c5485a3fd3810c68550b09938b6557dca1
Void8500 <alnimrawiqais@gmail.com>
2025-10-10 17:54:31 +0400

    Finished most of day6

---
+++ module-3/day-6/calendar.py
@@ -0,0 +1,6 @@
+import calendar
+
+yy = int(input("Enter a year: "))
+mm = int(input("Enter a month: "))
+
+print(calendar.month(yy, mm))
---
+++ module-3/day-6/date-time.py
@@ -0,0 +1,7 @@
+from datetime import date, time, datetime
+
+today = date.today()
+now = datetime.now()
+print("Date and time now is", now)
+print("The date of today is", today)
+print("The current year is", today.year)
