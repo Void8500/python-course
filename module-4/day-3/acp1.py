bf1eea80eccea4cb8c4d37e6e943ca58c7579166
Void8500 <alnimrawiqais@gmail.com>
2025-11-07 13:09:10 +0400

    Finished acp

---
+++ module-4/day-3/acp1.py
@@ -0,0 +1,12 @@
+test_dict = {'Codingal': 3, 'is': 2, 'best': 2, 'for': 2, 'Coding': 1}
+
+K = int(input("Enter the value to check frequency: "))
+
+print("The dictionary:", test_dict)
+
+search = 0
+for key in test_dict:
+    if test_dict[key] == K:
+        search += 1
+
+print(f"The value {K} has appeared {search} times.")
---
+++ module-4/day-4/acp1.py
@@ -0,0 +1,7 @@
+set1_a = {'blue', 'green'}
+set2_a = {'blue', 'yellow'}
+print("A. Symmetric Difference:", set1_a.symmetric_difference(set2_a))
+
+set1_b = {1, 2, 3, 4, 5}
+set2_b = {1, 5, 6, 7, 8, 9}
+print("B. Symmetric Difference:", set1_b.symmetric_difference(set2_b))
