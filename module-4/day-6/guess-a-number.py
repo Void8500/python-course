6484e90bf670e88982e17dc020bf7a73597b9c46
Void8500 <alnimrawiqais@gmail.com>
2025-11-07 18:08:36 +0400

    number game

--- module-4/day-6/guess-a-number.py
+++ module-4/day-6/guess-a-number.py
@@ -13,18 +13,19 @@ while True:
     else:
         print("Hint: The number is odd!")
     
-    for i in range(1,4):
+    for i in range(3):
         guess = int(input("Guess a number: "))
         
         if guess == number:
             print("You guessed correctly! The number was ", number)
+            break
         elif guess > number:
             print("Too High!")
-        elif guess < number:
+        else:
             print("Too low!")
     else:
         print("Sorry! You ran out of attempts. The number was", number)
-        play_again = input("Do you want to play again? (yes/no): ").lower()
-        if play_again != "yes":
-            print(f"Thanks for playing, {user}!")
-            break
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again != "yes":
+        print(f"Thanks for playing, {user}!")
+        break
