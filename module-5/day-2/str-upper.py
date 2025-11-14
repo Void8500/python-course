03beb8095e879194ba23d8a39152207108d8df19
Void8500 <alnimrawiqais@gmail.com>
2025-11-14 17:51:42 +0400

    Finished day2 mod5

---
+++ module-5/day-2/str-upper.py
@@ -0,0 +1,23 @@
+# create class
+class IOString():
+	# constructor to set default value
+    def __init__(self):
+        self.str1 = ""
+
+	# function to get input from user
+    def get_String(self):
+        self.str1 = input("Enter String : ")
+
+	# function to print the string in upper case
+    def print_String(self):
+        print("Result is :", self.str1.upper())
+
+# Object creation
+str1 = IOString()
+
+# Call functions
+str1.get_String()
+str1.print_String()
+
+str2 = str1.str1.lower()
+print(f"Result is: {str2}")
